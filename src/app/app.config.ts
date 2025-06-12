80e99e0ed23a72009fcc76a7640065ef51577398
Fxdonad <toquangduc2004@gmail.com>
2025-06-12 18:25:34 +0700

    refactor code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,51 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './services/config-service/error.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { authReducer } from './store/auth/auth.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { ReactiveFormsModule } from '@angular/forms';
+import { formReducer } from './store/open-form-state/form.reducer';
+import { notificationReducer } from './store/notification/notification.reducer';
+import { uiReducer } from './store/rerender/ui.reducer';
+import { tagFilmReducer } from './store/tag-film/tag-film.reducer';
+import { stringReducer } from './store/object-string/strings.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideAnimations(),
+
+    // Cấu hình Redux Store
+    provideStore({
+      auth: authReducer,
+      form: formReducer,
+      notification: notificationReducer,
+      uiState: uiReducer,
+      tagFilm: tagFilmReducer,
+      stringList: stringReducer,
+    }),
+
+    provideEffects(),
+    ReactiveFormsModule,
+  ],
 };
+
+// provideRouter(routes),
+// provideHttpClient(),
+// provideStore(),
+// provideEffects(),
+// provideStore({ auth: authReducer }),
+// provideState({ name: 'form', reducer: formReducer }),
+// provideAnimations(), //Cung cấp Animations cho Angular
+// provideStore({ notification: notificationReducer }),
+// provideStore({ uiState: uiReducer }),
+// provideState({ name: 'notification', reducer: notificationReducer }),
+// provideStore({ tagFilm: tagFilmReducer }),
+// provideHttpClient(withInterceptors([errorInterceptor])),
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { authReducer } from './app/store/auth/auth.reducer';
-import { formReducer } from './app/store/open-form-state/form.reducer';
-import { notificationReducer } from './app/store/notification/notification.reducer';
-import { uiReducer } from './app/store/rerender/ui.reducer';
-import { tagFilmReducer } from './app/store/tag-film/tag-film.reducer';
-import { errorInterceptor } from './app/services/config-service/error.interceptor';
-import { ReactiveFormsModule } from '@angular/forms';
-import { stringReducer } from './app/store/object-string/strings.reducer';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
-    provideAnimations(),
-
-    // Cấu hình Redux Store
-    provideStore({
-      auth: authReducer,
-      form: formReducer,
-      notification: notificationReducer,
-      uiState: uiReducer,
-      tagFilm: tagFilmReducer,
-      stringList: stringReducer,
-    }),
-
-    provideEffects(),
-    ReactiveFormsModule,
-  ],
-}).catch((err) => console.error(err));
-
-// provideRouter(routes),
-// provideHttpClient(),
-// provideStore(),
-// provideEffects(),
-// provideStore({ auth: authReducer }),
-// provideState({ name: 'form', reducer: formReducer }),
-// provideAnimations(), //Cung cấp Animations cho Angular
-// provideStore({ notification: notificationReducer }),
-// provideStore({ uiState: uiReducer }),
-// provideState({ name: 'notification', reducer: notificationReducer }),
-// provideStore({ tagFilm: tagFilmReducer }),
-// provideHttpClient(withInterceptors([errorInterceptor])),
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
